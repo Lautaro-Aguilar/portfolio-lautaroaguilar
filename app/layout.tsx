ff038501a110da8d2ab6fd3bd26e39930890e1c3
Lautaro Aguilar <aguilarlautaroexequiel867@gmail.com>
2024-02-02 10:41:50 -0300

    implement next-theme and theme-switch

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { ThemeProvider } from "@/components/theme-provider";
+import ThemeSwitch from "@/components/theme-switch";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,8 +18,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <ThemeSwitch />
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
---
+++ components/theme-switch.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useIsClient } from "usehooks-ts";
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function ThemeSwitch() {
+  const { setTheme, theme } = useTheme();
+  const isClient = useIsClient();
+
+  // create skeleton
+  if (!isClient)
+    return (
+      // skeleton
+      <Skeleton className="fixed bottom-5 right-5 w-[3rem] h-[3rem] rounded-full hover:scale-[1.15] active:scale-105 transition-all bg-black/30" />
+    );
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <button
+      className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
+      onClick={() => toggleTheme()}
+    >
+      {theme === "light" ? <Sun /> : <Moon />}
+    </button>
+  );
+}
